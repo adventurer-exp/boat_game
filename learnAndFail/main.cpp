e8ef61bf0c644bb1a32c802073caa1ebb1d9d2b3
Simona Bilkova <sbilkova99@gmail.com>
2019-12-07 17:58:42 +1100

    Initial Commit

---
+++ learnAndFail/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnAndFail
+//
+//  Created by Simona Bilkova on 7/12/19.
+//  Copyright © 2019 Simona Bilkova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
